837623f6116d3c9cff1bcf188f6e9d69063fbcad
ghana <ghana.chindukuri@gmail.com>
2021-04-24 10:34:28 +0530

    🔥 Deploying to netlify

---
+++ src/components/IndividualProject.js
@@ -0,0 +1,46 @@
+import React, {useState} from 'react'
+import {FaTrashAlt} from 'react-icons/fa'
+import PropTypes from 'prop-types'
+import {useProjectsValue, useSelectedProjectValue} from '../context'
+import {firebase} from '../firebase'
+
+export const IndividualProject = ({project}) => {
+	const [showConfirm, setShowConfirm] = useState(false)
+	const {projects, setProjects} = useProjectsValue()
+	const {setSelectedProject} = useSelectedProjectValue()
+
+	const deleteProject = docId => {
+		firebase
+			.firestore()
+			.collection('projects')
+			.doc(docId)
+			.delete()
+			.then(() => {
+				setProjects([...projects])
+				setSelectedProject('INBOX')
+			})
+	}
+
+	return (
+		<>
+			<span className='sidebar__dot'>•</span>
+			<span className='sidebar__project-name'>{project.name}</span>
+			<span
+				className='sidebar__project-delete'
+				data-testid='delete-project'
+				onClick={() => deleteProject(project.docId)}
+				onKeyDown={e => {
+					if (e.key === 'Enter') setShowConfirm(!showConfirm)
+				}}
+				tabIndex={0}
+				role='button'
+				aria-label='Confirm deletion of project'>
+				<FaTrashAlt />
+			</span>
+		</>
+	)
+}
+
+IndividualProject.propTypes = {
+	project: PropTypes.object.isRequired,
+}
